32fcf5ea2436eff8321d87a8b2243d96b61f97d2
DanielKhine <danielrio126@gmail.com>
2020-09-12 16:44:32 +0630

    small update

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,30 @@
 import React from "react"
+import * as Font from "expo-font"
 import { View, Text, TouchableOpacity } from "react-native"
 
 import styles from "./styles"
+import { loadAsync } from "expo-font"
 
 export default class SearchScreen extends React.Component {
+  state = {
+    isReady: false
+  }
+  loadFontsAsync = async () => {
+    await loadAsync({
+      Kufam: require("../../assets/fonts/Kufam-Regular.ttf")
+    })
+    this.setState({ isReady: true })
+  }
+
+  componentWillMount() {
+    this.loadFontsAsync()
+  }
+
   render() {
-    return (
+    return this.state.isReady ? (
       <View style={styles.container}>
-        <Text>Search Screen</Text>
+        <Text style={{ fontSize: 22, fontFamily: "Kufam" }}>Search Screen</Text>
       </View>
-    )
+    ) : null
   }
 }
